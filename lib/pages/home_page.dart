e138d5d38fb9eadb66dad1d7f4a362ed48c58d8b
ALI HASAN KAZMI <alihassankazmi14@gmail.com>
2023-07-21 12:47:51 +0500

    DAY 11 WORK HAS BEEN COMPLETED

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/models/catalog.dart';
+import 'package:flutter_course/widgets/item_widget.dart';
 import 'package:flutter_course/widgets/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,6 +8,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(6, (index) => CatalogModel().items[0]);
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
@@ -13,7 +16,18 @@ class HomePage extends StatelessWidget {
           "Catalog App",
         ),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          // itemCount: CatalogModel().items.length,
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
+      ),
       drawer: const MyDrawer(),
     );
   }
